22de50355466ef903001914a3c8b96253b34162b
CodeMegan27 <mmeganathan827@gmail.com>
2024-09-09 16:11:09 +0530

    Next Birthday

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function startConfetti() {
 }
 
 function updateCountdown() {
-    const endDate = new Date('2024-09-09T00:00:00'); // Replace with your desired date and time
+    const endDate = new Date('2025-09-09T00:00:00'); // Replace with your desired date and time
     const now = new Date();
     const timeDifference = endDate - now;
 
@@ -63,6 +63,6 @@ function updateCountdown() {
 }
 
 updateCountdown()
-setInterval(startConfetti, 7000); // Burst confetti every 5 seconds
+// setInterval(startConfetti, 7000); // Burst confetti every 5 seconds
 setInterval(updateCountdown, 1000);
-startConfetti()
+// startConfetti()
